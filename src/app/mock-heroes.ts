f80a34028dcc523f10c5f84475874bdbdf029854
MarcoAnt1 <54957584+MarcoAnt1@users.noreply.github.com>
2020-11-20 18:13:09 -0300

    Display a List

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Dragon Hero Ryuku' },
+    { id: 12, name: 'Equipped Hero Yori Musha' },
+    { id: 13, name: 'Wash' },
+    { id: 14, name: 'Kamui Woods' },
+    { id: 15, name: 'Crust' },
+    { id: 16, name: 'Mirko' },
+    { id: 17, name: 'Edgeshot' },
+    { id: 18, name: 'Best Jeanist' },
+    { id: 19, name: 'Hawks' },
+    { id: 20, name: 'Endeavor' },
+    { id: 21, name: 'All Might' },
+]
